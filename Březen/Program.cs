d206be3ec9f597e708abb1707413968fc94f4973
kubik <kubik@DESKTOP-2J3139V>
2022-06-23 16:32:53 +0200

    Přidejte soubory projektu.

---
+++ Březen/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Napiš první číslo: ");
+int cislo1 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write("Napiš druhé číslo: ");
+int cislo2 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+if (cislo1 > cislo2)
+{
+    Console.WriteLine($"Číslo {cislo1} je větší než číslo {cislo2}");
+}
+else
+{
+    Console.WriteLine($"Číslo {cislo2} je větší než číslo {cislo1}");
+}
